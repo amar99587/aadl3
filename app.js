013117702e448476513180733c0a6346befe470b
amar99587 <72984889+amar99587@users.noreply.github.com>
2024-07-08 09:40:28 +0100

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,18 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.static('public'));
 
+// Configure CORS middleware
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Replace * with your allowed origins in production
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Include Content-Type here
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200); // Respond to OPTIONS requests with 200 OK
+  } else {
+    next(); // Pass control to the next middleware
+  }
+});
+
 // PostgreSQL setup
 const db = new Pool({
   connectionString: "postgresql://aadl3_user:jxq9LlVDL9B9f9TyhhfMiukZUZLzrP1w@dpg-cq5g4508fa8c7386vpmg-a/aadl3",
