b1c883f3c30923ad229c1c6fedf82ad1e7d7e1c4
amar99587 <72984889+amar99587@users.noreply.github.com>
2024-07-08 01:13:20 +0100

    Update cron.js

--- cron.js
+++ cron.js
@@ -3,9 +3,20 @@ const axios = require('axios');
 const { Pool } = require('pg');
 
 // Firebase setup
-const serviceAccount = require("./aadl-3-2002ed556a3e.json");
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert({
+    type: process.env.type,
+    project_id: process.env.project_id,
+    private_key_id: process.env.private_key_id,
+    private_key: process.env.private_key,
+    client_email: process.env.client_email,
+    client_id: process.env.client_id,
+    auth_uri: process.env.auth_uri,
+    token_uri: process.env.token_uri,
+    auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+    client_x509_cert_url: process.env.client_x509_cert_url,
+    universe_domain: process.env.universe_domain
+  })
 });
 
 // PostgreSQL setup
